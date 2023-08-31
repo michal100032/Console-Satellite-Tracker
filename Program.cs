35fcbe2f3e1e25f2e5f122a24138b89be69c5231
Michał <michal100032@gmail.com>
2023-08-31 17:27:15 +0200

    Dodaj pliki projektów.

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Console_Satellite_Tracker {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
